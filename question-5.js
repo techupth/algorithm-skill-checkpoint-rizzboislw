316033bf02f045b910c5a554d4b987e40dbd0794
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-07-30 09:32:40 +0700

    question-5-finished

--- question-5.js
+++ question-5.js
@@ -2,5 +2,7 @@
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
 Answer:
 
+    Big O ของ function sortProductsByPrice คือ O(n^2) เนื่องจากมีการใช้ Bubble Sort หรือก็คือ nested loop นั่นเอง 
+    ซึ่งแต่ละ loop จะเป็น linear search ที่เป็น O(n) เมื่อเป็น loop ซ้อนกัน 2 อันจึงทำให้เป็น O(n^2)
 
 */
